318268b49cc02245ac5ff4822183d4b614c7bf0b
Christopher Beloch <chris@somekindofcode.com>
2020-10-12 19:41:54 +0200

    initial screenshot downloader

---
+++ run.py
@@ -0,0 +1,78 @@
+import sys
+import os
+import asyncio
+import re
+
+from aiohttp import ClientResponseError, ClientSession
+
+from xbox.webapi.api.client import XboxLiveClient
+from xbox.webapi.authentication.manager import AuthenticationManager
+from xbox.webapi.common.exceptions import AuthenticationException
+
+from xbox.webapi.authentication.models import OAuth2TokenResponse
+
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+TOKEN_DIR = "/Users/cbeloch/Library/Application Support/xbox/tokens.json"
+
+TARGET_DIR = "./Games"
+
+def clean_game_title(title: str) -> str:
+    return re.sub("[^\d\w\-_\s]", '', title)
+
+async def download(uri: str, destination: str, appendExtension: bool = False):
+    async with ClientSession() as session:
+        async with session.get(uri) as resp:
+            FILE_DEST = destination
+
+            if appendExtension:
+                components = resp.url.name.split(".")
+                FILE_DEST = "%s.%s" % (destination, components[-1])
+
+            data = await resp.read()
+
+            with open(FILE_DEST, "wb") as f:
+                f.write(data)
+
+
+async def run(): 
+    async with ClientSession() as session:
+        # Setup Authentication
+
+        auth_mgr = AuthenticationManager(
+            session, CLIENT_ID, CLIENT_SECRET, ""
+        )
+
+        with open(TOKEN_DIR, mode="r") as f:
+            tokens = f.read()
+        auth_mgr.oauth = OAuth2TokenResponse.parse_raw(tokens)
+        try:
+            await auth_mgr.refresh_tokens()
+        except ClientResponseError:
+            print("Could not refresh tokens")
+            sys.exit(-1)
+
+        with open(TOKEN_DIR, mode="w") as f:
+            f.write(auth_mgr.oauth.json())
+
+        # Setup Xbox API Client
+        xbl_client = XboxLiveClient(auth_mgr)
+
+        # Get Screenshots
+        screenshotResponse = await xbl_client.screenshots.get_saved_own_screenshots(max_items = 100)
+
+        # Iterate screenshots
+        for screenshot in screenshotResponse.screenshots:
+            TARGET_DIR_GAME = "%s/%s" % (TARGET_DIR, clean_game_title(screenshot.title_name))
+            
+            # Create Game Folder if it does not exist
+            os.makedirs(TARGET_DIR_GAME, exist_ok=True)
+
+            SCREENSHOT_DESTINATION = "%s/%s" % (TARGET_DIR_GAME, screenshot.date_taken.strftime("%Y-%m-%d_%H-%M-%S"))
+            
+            await download(screenshot.screenshot_uris[0].uri, SCREENSHOT_DESTINATION, True)
+
+# RUN
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+result = loop.run_until_complete(run())
