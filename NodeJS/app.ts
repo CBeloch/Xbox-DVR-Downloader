f4c647fab08ddc7836ae014768cfa9fe3fda944e
Christopher Beloch <chris@somekindofcode.com>
2020-10-15 16:54:41 +0200

    added download code for the typescript version

--- NodeJS/app.ts
+++ NodeJS/app.ts
@@ -1,7 +1,36 @@
-import XRay from "x-ray";
+import XRay from "x-ray"
+import { default as fs } from 'fs'
+import { default as fetch } from 'node-fetch'
+
+
+const TARGET_DIR = "./Games"
+
+let x = XRay({
+    filters: {
+        dvrDate: function(value: string) {
+            let dateString = value.trim().match(/\d{2}\/\d{2}\/\d{4}/)
+            return dateString ? new Date(dateString[0]) : value
+        },
+        formattedDate: function(value: string) {
+            try {
+                let date = new Date(value)
+                return date.toISOString()
+                    .replace(/T/g, '_')
+                    .replace(/:/g, '-')
+                    .replace(/Z/g, '')
+                    .split('.')
+                    [0]
+            } catch {
+                return value
+            }
+        }
+    }
+})
+
 
 interface ScreenshotData {
     game_title: string
+    dateTime: string
     link: string
     download: string
 }
@@ -11,21 +40,57 @@ function clean_game_title(title: string): string {
     return title.replace(regex, '')
 }
 
-async function run() {
-    let x = XRay()
-    
-    let screenshots: [ScreenshotData] = await x('https://gamerdvr.com/gamer/cbeloch/screenshots', 'ul.slideshow-image-viewer li', [
+async function download(urlString: string, destination: string, filename: string | null) {
+    fs.mkdirSync(destination, { recursive: true })
+
+    let url = new URL(urlString)
+    let pathComponents = url.pathname.split('/')
+    let baseName = pathComponents[pathComponents.length - 1]
+    let baseNameParts = baseName.split('.')
+    let fileExtension = baseNameParts[baseNameParts.length - 1]
+
+    let fullfilename = filename ? `${filename}.${fileExtension}` :  baseName
+
+    let fileDestination = `${destination}/${fullfilename}`
+
+    if (fs.existsSync(fileDestination)) {
+        console.log(`${fileDestination} already exists`)
+    }
+
+    let response = await fetch(urlString)
+
+    if (response.ok) {
+
+        let fileStream = fs.createWriteStream(fileDestination);
+        response.body.pipe(fileStream)
+        console.log(`Downloaded ${fileDestination}`)
+
+    } else {
+        console.log(`Error downloading - Status code: ${response.status}`) 
+    }
+}
+
+async function getScreenshots(): Promise<[ScreenshotData]> {
+    return await x('https://gamerdvr.com/gamer/cbeloch/screenshots', 'ul.slideshow-image-viewer li', [
         {
             game_title: '.top-row a',
+            dateTime: x('.content-row a@href', '.toggle-details time | formattedDate'),
             link: '.content-row a@href',
             download: '@data-x-s'
         }
     ]).paginate('ul.pagination .next a@href')
-    .limit(2)
+    .limit(1)
+}
 
-    screenshots.forEach((screen) => {
-        console.log(`${screen.game_title} <|> ${clean_game_title(screen.game_title)}`)
-    })
+async function run() {
+    let screenshots = await getScreenshots()
+
+    console.log(screenshots)
+
+    for (const screen of screenshots)  {
+        let save_dir = `${TARGET_DIR}/${clean_game_title(screen.game_title)}`
+        await download(screen.download, save_dir, screen.dateTime)
+    }
 }
 
 run()
